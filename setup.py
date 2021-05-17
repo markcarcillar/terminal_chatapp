9d6fe616fbbea356bb89f60c79f3bd7b2f3c9b38
ace computer <markcarcillar04@protonmail.com>
2021-05-17 14:40:45 +0800

    Added more details for setting up this project on setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+with open('README.md') as readmefile:
+    long_description = readmefile.read()
+
 setup(
     name='terminal-chatapp',
     version='1.0.0',
+    author='Mark Carcillar',
+    description='Secure communication on terminal.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/clediscover/terminal_chatapp',
     packages=['terminal_chatapp'],
     install_requires = [
         'websockets',
         'cryptography'
-    ]
+    ],
+    python_requires='>=3.6'
 )
